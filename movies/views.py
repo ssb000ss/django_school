84c54f6599d08b34f479b62021e5bfe711be881e
ssb000ss <ssb000ss@gmail.com>
2020-08-17 15:17:31 +0600

    devide into to Jinja blocks

--- movies/views.py
+++ movies/views.py
@@ -7,10 +7,10 @@ from .models import Movie
 
 class MoviesView(ListView):
     """Список фильмов"""
-
     model = Movie
     queryset = Movie.objects.filter(draft=False)
     template_name = "movies/movie_list.html"
+
 
 class MovieDetailView(DetailView):
     """Полное описание фильма"""
